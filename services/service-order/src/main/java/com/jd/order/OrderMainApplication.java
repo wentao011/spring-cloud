5d6131abaebf3278d2f8e090653e22f143449123
test001 <3028001994@qq.com>
2025-08-06 00:11:23 +0800

    nacos简单服务注册

---
+++ services/service-order/src/main/java/com/jd/order/OrderMainApplication.java
@@ -0,0 +1,16 @@
+package com.jd.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: OrderApplication主程序
+ * @author: -
+ * @date: 2025-08-05
+ **/
+@SpringBootApplication
+public class OrderMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMainApplication.class, args);
+    }
+}
---
+++ services/service-product/src/main/java/com/jd/product/ProductMainApplication.java
@@ -0,0 +1,16 @@
+package com.jd.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: ProductApplication主程序
+ * @author: -
+ * @date: 2025-08-05
+ **/
+@SpringBootApplication
+public class ProductMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMainApplication.class, args);
+    }
+}
