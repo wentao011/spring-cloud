336a302a3fd3bb7f03839b7938caeacf4f36e72f
test001 <3028001994@qq.com>
2025-08-11 23:40:14 +0800

    sentinel-02 规则（流控、熔断降级、热点参数）

--- services/service-order/src/main/java/com/jd/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/jd/order/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.jd.order.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.jd.bean.Order;
 import com.jd.order.properties.OrderProperties;
 import com.jd.order.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 // @RefreshScope + @Value可以实现配置动态刷新
 // @RefreshScope
+@Slf4j
 @RestController
 public class OrderController {
     @Autowired
@@ -39,6 +42,48 @@ public class OrderController {
     @GetMapping(value = "/create")
     public Order createOrder(@RequestParam("userId") Long userId, @RequestParam("productId") Long productId) {
         Order order = orderService.createOrder(userId, productId);
+        return order;
+    }
+
+    @GetMapping("/writeDb")
+    public String writeDb() {
+        return "writeDb success...";
+    }
+
+    @GetMapping("/readDb")
+    public String readDb() {
+        log.info("readDb success...");
+        return "readDb success...";
+    }
+
+    // @GetMapping("/seckill")
+    // public Order seckill(@RequestParam("userId") Long userId,
+    //                      @RequestParam("productId") Long productId) {
+    //     Order order = orderService.createOrder(userId, productId);
+    //     order.setId(Long.MAX_VALUE);
+    //     return order;
+    // }
+
+    @GetMapping("/seckill")
+    @SentinelResource(value = "seckill-order", fallback = "seckillFallback") //此处用于测试热点规则
+   // public Order seckill(@RequestParam("userId") Long userId,
+   //                      @RequestParam("productId") Long productId) {
+//此处模拟不传参数，给默认参数
+//    public Order seckill(@RequestParam(value = "userId",defaultValue = "888") Long userId,
+//                         @RequestParam(value = "productId",defaultValue = "1000") Long productId) {
+    public Order seckill(@RequestParam(value = "userId", required = false) Long userId,
+                         @RequestParam(value = "productId", required = false) Long productId) {
+        Order order = orderService.createOrder(userId, productId);
+        order.setId(Long.MAX_VALUE);
+        return order;
+    }
+
+    public Order seckillFallback(Long userId, Long productId, Throwable exception) {
+        System.out.println("seckillFallback...");
+        Order order = new Order();
+        order.setId(productId);
+        order.setUserId(userId);
+        order.setAddress("seckillFallback异常信息" + exception.getClass());
         return order;
     }
 }
--- services/service-product/src/main/java/com/jd/product/controller/ProductController.java
+++ services/service-product/src/main/java/com/jd/product/controller/ProductController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class ProductController {
     @Autowired
@@ -17,11 +19,18 @@ public class ProductController {
     public Product getProductById(@PathVariable("id") Long productId,
                                   HttpServletRequest httpServletRequest) {
         System.out.println("正在远程调用service-product...");
-
         String XToken = httpServletRequest.getHeader("X-Token");
         System.out.println("X-Token:" + XToken);
-
         Product product = productService.getProductById(productId);
+
+        // try {
+        //     TimeUnit.SECONDS.sleep(2);
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+
+        // int i = 10 / 0;
+
         return product;
     }
 }
